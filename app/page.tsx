41ae54bac9535575bbf0273be7d3eb0aea54bcff
deiv-hash <fantachat88@gmail.com>
2025-02-12 00:42:36 +0100

    Add social icons and wallet connect button to header

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { TwitterIcon } from "./components/icons/TwitterIcon";
+import { DiscordIcon } from "./components/icons/DiscordIcon";
 
 export default function Home() {
   const pathname = usePathname();
@@ -11,13 +13,16 @@ export default function Home() {
       <header className="">
         <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
           <div className="flex items-center space-x-8">
-            <Image
-              src="/logo.png"
-              alt="Potion Leaderboard"
-              width={100}
-              height={100}
-              className="w-10 h-10"
-            />
+            <Link href="/">
+              <Image
+                src="/logo.png"
+                alt="Potion Leaderboard"
+                width={100}
+                height={200}
+                className=""
+              />
+            </Link>
+
             <nav className="flex space-x-4">
               <Link
                 href="/"
@@ -37,6 +42,27 @@ export default function Home() {
                 Prizes
               </a>
             </nav>
+          </div>
+          <div className="flex items-center space-x-4 text-white">
+            <a
+              href="https://twitter.com/potionleaderboard"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:text-white"
+            >
+              <TwitterIcon />
+            </a>
+            <a
+              href="https://discord.gg/potionleaderboard"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:text-white"
+            >
+              <DiscordIcon />
+            </a>
+            <button className="bg-[#AA00FF] px-4 py-2 rounded-lg hover:bg-purple-600">
+              Connect Wallet
+            </button>
           </div>
         </div>
       </header>
