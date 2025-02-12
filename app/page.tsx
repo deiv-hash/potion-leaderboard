1659de78c7dcfd2f5914520635c56f60406cb055
deiv-hash <fantachat88@gmail.com>
2025-02-12 12:03:47 +0100

    Add initial tab navigation buttons to main page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,14 @@ export default function Home() {
     <div className="min-h-screen">
       <div className="max-w-7xl mx-auto">
         <Header />
+        <main>
+          <div className="flex justify-center items-center">
+            <div className="text-gray-400">
+              <button className="btn-tab">Traders</button>
+              <button>Groups</button>
+            </div>
+          </div>
+        </main>
       </div>
     </div>
   );
