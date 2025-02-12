696fcaa1cf3b7baf123023d78da898659b1c688a
deiv-hash <fantachat88@gmail.com>
2025-02-12 12:51:03 +0100

    Add Trader interface type definition

--- types/trader.ts
+++ types/trader.ts
@@ -1,1 +1,18 @@
 export type TimeFrame = "daily" | "weekly" | "monthly" | "all-time";
+
+export interface Trader {
+  id: string;
+  name: string;
+  wallet: string;
+  image: string;
+  rank: number;
+  xFollowers: number;
+  xTag: string;
+  tokensTraded: number;
+  winRate: number;
+  tradesCount: { buy: number; sell: number };
+  avgBuy: { solAmount: number; usdAmount: number };
+  avgEntry: number;
+  avgHold: number;
+  realizedPnl: { solAmount: number; usdAmount: number };
+}
